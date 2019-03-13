8aac454e03231218de31fd39e60c2e91845356d0
Glitch (db-tr) <none>
2019-03-13 16:27:05 +0000

    🌷🎀 Checkpoint ./html/api.ejs:1643465/3 ./html/bot.ejs:1643465/1 ./html/botekle.ejs:1643465/1 ./html/botlar.ejs:1643465/1 ./html/duzenle.ejs:1643465/1 ./html/hakkımızda.ejs:1643465/1 ./html/hata.ejs:1643465/1 ./html/hataa.ejs:1643465/3 ./html/hataaa.ejs:1643465/1 ./html/hate.ejs:1643465/1 ./html/index.ejs:1643465/1 ./html/kullanici.ejs:1643465/1 ./html/p-ayarla.ejs:1643465/1 ./html/panel.ejs:1643465/1 ./html/profil.ejs:1643465/1 ./html/rapor.ejs:1643465/1 ./html/reddet.ejs:1643465/1 ./html/sertifika.ejs:1643465/1 ./html/y-panel.ejs:1643465/1 ./html/sil.ejs:1643465/1 ./komutlar/yardım.js:1643465/5422

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,12 +4,12 @@ exports.run = async (client, message, args) => {
 	//kendi URL'lerini eklersn :D
   var embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-  .setAuthor("DiscordBotsTR", client.user.avatarURL)
-  .addField("Sistemin amacı nedir? Sistem ne işe yaramaktadır?", "Gerekli bilgiyi [buraya tıklayarak](https://discordbotstr.xyz/hakkimizda) görebilirsiniz.")
-	.addField("Bot nasıl ekleyeceğim?", "[Buraya tıklayarak](https://discordbotstr.xyz/botekle) bot ekleyebileceğiniz yere ışınlanabilirsiniz.")
-  .addField("Sistemdeki tüm botları nereden görebilirim?", "[Buraya tıklayarak](https://discordbotstr.xyz/botlar) görebilirsiniz.")
-  .addField("Sertifika ne işe yarar? Nasıl alınır?", "[Buraya tıklayarak](https://discordbotstr.xyz/sertifika) görebilirsiniz.")
-  .addField("Kullanıcı panelinde neler yapılabilir?", "Kullanıcı panelinde sistemdeki botlarınızın profilini/başvurusunu düzenleyebilir ve botlarınızı sistemden silebilirsiniz. \nKullanıcı paneline gitmek için [buraya tıklayabilirsiniz.](https://discordbotstr.xyz/panel)")
+  .setAuthor("World Bot List", client.user.avatarURL)
+  .addField("Sistemin amacı nedir? Sistem ne işe yaramaktadır?", "Gerekli bilgiyi [buraya tıklayarak](https://worldbotlist.glitch.me/hakkimizda) görebilirsiniz.")
+	.addField("Bot nasıl ekleyeceğim?", "[Buraya tıklayarak](https://worldbotlist.glitch.me/botekle) bot ekleyebileceğiniz yere ışınlanabilirsiniz.")
+  .addField("Sistemdeki tüm botları nereden görebilirim?", "[Buraya tıklayarak](https://worldbotlist.glitch.me/botlar) görebilirsiniz.")
+  .addField("Sertifika ne işe yarar? Nasıl alınır?", "[Buraya tıklayarak](https://worldbotlist.glitch.me/sertifika) görebilirsiniz.")
+  .addField("Kullanıcı panelinde neler yapılabilir?", "Kullanıcı panelinde sistemdeki botlarınızın profilini/başvurusunu düzenleyebilir ve botlarınızı sistemden silebilirsiniz. \nKullanıcı paneline gitmek için [buraya tıklayabilirsiniz.](https://worldbotlist.glitch.me/panel)")
   message.channel.send({embed: embed})
   
 };
