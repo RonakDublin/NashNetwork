bf933a0e260e93d553d430b9234195ae66dee65f
Glitch (db-tr) <none>
2019-03-19 08:42:54 +0000

    🙏🐄 Checkpoint ./komutlar/yardım.js:1643465/55 ./app.js:1643465/11 ./html/css/genel.css:1643465/361

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,11 +5,11 @@ exports.run = async (client, message, args) => {
   var embed = new Discord.RichEmbed()
   .setColor('RANDOM')
   .setAuthor("World Bot List", client.user.avatarURL)
-  .addField("Sistemin amacı nedir? Sistem ne işe yaramaktadır?", "Gerekli bilgiyi [buraya tıklayarak](https://worldbotlist.glitch.me/hakkimizda) görebilirsiniz.")
-	.addField("Bot nasıl ekleyeceğim?", "[Buraya tıklayarak](https://worldbotlist.glitch.me/botekle) bot ekleyebileceğiniz yere ışınlanabilirsiniz.")
-  .addField("Sistemdeki tüm botları nereden görebilirim?", "[Buraya tıklayarak](https://worldbotlist.glitch.me/botlar) görebilirsiniz.")
-  .addField("Sertifika ne işe yarar? Nasıl alınır?", "[Buraya tıklayarak](https://worldbotlist.glitch.me/sertifika) görebilirsiniz.")
-  .addField("Kullanıcı panelinde neler yapılabilir?", "Kullanıcı panelinde sistemdeki botlarınızın profilini/başvurusunu düzenleyebilir ve botlarınızı sistemden silebilirsiniz. \nKullanıcı paneline gitmek için [buraya tıklayabilirsiniz.](https://worldbotlist.glitch.me/panel)")
+  .addField("Sistemin amacı nedir? Sistem ne işe yaramaktadır?", "Gerekli bilgiyi [buraya tıklayarak](https://worldbotlist.ml/hakkimizda) görebilirsiniz.")
+	.addField("Bot nasıl ekleyeceğim?", "[Buraya tıklayarak](https://worldbotlist.ml/botekle) bot ekleyebileceğiniz yere ışınlanabilirsiniz.")
+  .addField("Sistemdeki tüm botları nereden görebilirim?", "[Buraya tıklayarak](https://worldbotlist.ml/botlar) görebilirsiniz.")
+  .addField("Sertifika ne işe yarar? Nasıl alınır?", "[Buraya tıklayarak](https://worldbotlist.ml/sertifika) görebilirsiniz.")
+  .addField("Kullanıcı panelinde neler yapılabilir?", "Kullanıcı panelinde sistemdeki botlarınızın profilini/başvurusunu düzenleyebilir ve botlarınızı sistemden silebilirsiniz. \nKullanıcı paneline gitmek için [buraya tıklayabilirsiniz.](https://worldbotlist.ml/panel)")
   message.channel.send({embed: embed})
   
 };
